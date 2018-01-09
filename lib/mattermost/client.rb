8efab019d8f5626a99fb031e42e528d7120af57a
Josh Brody <josh@josh.mn>
2018-01-09 13:55:34 -0600

    remove binding.pry

--- lib/mattermost/client.rb
+++ lib/mattermost/client.rb
@@ -6,7 +6,6 @@ module Mattermost
 
     def self.connect!
       req = HTTParty.post("#{Mattermost.configuration.protocol}://#{Mattermost.configuration.server}/api/v4/users/login", :body => {:login_id => Mattermost.configuration.username, :password => Mattermost.configuration.password}.to_json)
-      binding.pry
       if req.success?
         class_eval do
           base_uri "#{Mattermost.configuration.protocol}://#{Mattermost.configuration.server}/api/v4"
