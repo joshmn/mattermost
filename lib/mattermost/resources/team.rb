0795c7937d469492f102eab201aac220c79c49f2
Josh Brody <josh@josh.mn>
2018-01-09 14:14:43 -0600

    docs

--- lib/mattermost/resources/team.rb
+++ lib/mattermost/resources/team.rb
@@ -3,18 +3,49 @@ module Mattermost
 
     alias_method :team_id, :id
 
+    # Returns the given team
+    #   Team.find("long-team-id").raw
     get :raw, :as => :member, :endpoint => ''
-    get :stats, :as => :member
-    get :by_name, :as => :self, :endpoint => 'name/:team_name', :args => :team_name
-    get :exists, :as => :self, :endpoint => 'name/:team_name/exists', :args => :team_name
-    get :all, :as => :self, :endpoint => ''
-    post :create, :as => :self, :endpoint => ''
-    post :search, :as => :self, :body => :term
-    get :members, :as => :member
-    post :add_member, :as => :member, :endpoint => 'members'
-    get :member_by_user_id, :as => :member, :endpoint => 'members/:user_id', :args => :user_id
-    put :update_attributes, :as => :member, :endpoint => 'patch'
 
+    # Returns stats of a team
+    #   Team.find("long-team-id").stats
+    get :stats, :as => :member
+
+    # Returns a user by their name
+    #   Team.by_name("development")
+    get :by_name, :as => :self, :endpoint => 'name/:team_name', :args => :team_name
+
+    # Returns whether or not the team exists
+    #   Team.exists("party-planning-committee")
+    get :exists, :as => :self, :endpoint => 'name/:team_name/exists', :args => :team_name
+
+    # Returns all the teams on the instance
+    #   Team.all
+    get :all, :as => :self, :endpoint => ''
+
+    # Creates a team
+    #   Team.create(:name => "party-planning-committee", :display_name => "Party Planning Committee", type => 'I')
+    post :create, :as => :self, :endpoint => ''
+
+    # Search teams
+    #   Team.search("party")
+    post :search, :as => :self, :body => :term
+
+    # Gets members of a team
+    #   Team.find("long-team-id").members
+    get :members, :as => :member
+
+    # Adds a user to a Team
+    # Team.find("long-team-id").add_member(:team_id => "long-team-id", :user_id => "long-user-id", :roles => "team_admin")
+    post :add_member, :as => :member, :endpoint => 'members'
+
+    # Gets the member status of a user of a particiular team
+    # Team.find("long-team-id").member_by_user_id("long-user-id")
+    get :member_by_user_id, :as => :member, :endpoint => 'members/:user_id', :args => :user_id
+
+    # Updates a team
+    # Team.find("long-team-id").update_attributes(:display_name => "Former Party Planning Committee")
+    put :update_attributes, :as => :member, :endpoint => 'patch'
 
   end
 end
--- lib/mattermost/resources/user.rb
+++ lib/mattermost/resources/user.rb
@@ -2,27 +2,83 @@ module Mattermost
   class User < BaseResource
 
     alias_method :user_id, :id
+
+    # Creates a user
+    #   User.create(:email => "ryan@dundermifflin.com", :username => "ryanhoward", :password => "ilovekelly")
     post :create, :as => :self, :endpoint => ''
+
+    # Gets all users
+    #   User.all
     get :all, :as => :self, :endpoint => ''
+
+    # Gets users by their user IDs
+    #   User.by_ids("long-user-id-1", "long-user-id-2")
     post :by_ids, :as => :self, :endpoint => 'ids'
+
+    # Gets users by their usernames
+    #   User.by_usernames("long-username-1", "long-username-2")
     post :by_usernames, :as => :self, :endpoint => 'usernames'
+
+    # Opens an autocomplete endpoint
+    # Requires :name; optional params are :team_id, :channel_id
+    # :name can be a username, nickname, first name, or last name
+    #   User.autocomplete(:name => "something")
     get :autocomplete, :as => :self
+
+    # Returns the given user
+    #   User.find("long-user-id").raw
     get :raw, :as => :member, :endpoint => ''
+
+    # Updates a user
+    #   User.find("long-user-id").update_attributes(:email => "ceo@dundermifflin.com")
     put :update_attributes, :as => :member, :endpoint => 'patch'
+
+    # Updates the user's roles
+    #   User.find("long-user-id").roles("team_admin")
     put :roles, :as => :member, :endpoint => 'roles', :args => :roles
+
+    # Gets the member's avatar/display pic thing
+    #   User.find("long-user-id").image
     get :image, :as => :member
+
+    # Gets users by their username
+    #   User.by_username("ryan")
     get :by_username, :as => :self, :endpoint => 'username/:username'
+
+    # Gets users with a matching email
     get :by_email, :as => :self, :endpoint => 'email/:email'
+
+    # Gets current user sessions
     get :sessions, :as => :member
-    post :revoke, :as => :member, :endpoint => 'sessions/revoke', :args => :session_id
-    post :revoke_all, :as => :member, :endpoint => 'sessions/revoke'
+
+    # Revokes a given session
+    # User.find("long-user-id").revoke("session-id")
+    post :revoke_session, :as => :member, :endpoint => 'sessions/revoke', :args => :session_id
+
+    # Revokes all sessionds
+    post :revoke_all_sessions, :as => :member, :endpoint => 'sessions/revoke'
+
+    # Does an audit on the user
     get :audits, :as => :member
-    post :verify, :as => :self, :endpoint => 'email/verify'
+
+    # Verify the email used by a user to sign-up with
+    # User.verify("token")
+    post :verify_email, :as => :self, :endpoint => 'email/verify'
+
+    # Resend verification email
+    # User.send_verification_email("ryan@dundermifflin.com")
     post :send_verification_email, :as => :self, :endpoint => 'email/verify/send', :body => :email
 
+    # Get the teams a user is a member of
+    # User.find("long-user-id").teams
     get :teams, :as => :member
+
+    # ...
     get :team_members, :as => :member, :endpoint => 'teams/members'
 
+
+
+    # These probably don't work
     post :search, :as => :self, :endpoint => 'search' # this takes lots of args, should probably look at it
     post :image, :as => :member # i haven't done files yet...this probably won't work
   end
